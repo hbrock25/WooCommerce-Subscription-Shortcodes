297e05a866734c0b06e9c288f1b99d5ff782d1cf
Hugh Brock <hbrock@gmail.com>
2020-07-11 15:25:28 -0400

    Naturally a Subscription has 1..n products, so find if there is a sku that matches among the products, then return that.

--- includes/class-wcs-shortcodes.php
+++ includes/class-wcs-shortcodes.php
@@ -1012,7 +1012,8 @@ class WCSS_Shortcodes {
 			$ID = $item->get_product()->get_id();
 			if ($ID == 226548 || $ID == 225362 || $ID == 43122 ) {
 			    echo 'Your HCA membership expires on '
-			       . date(get_option('date_format'), $subscription->get_date( 'next_payment' ))
+			    //			       . date(get_option('date_format'), $subscription->get_date( 'next_payment' ))
+			       . $subscription->get_date( 'next_payment' )
 			       . '. | <a href="/academy/current-members">Renew</a> | '
 			       . '<a href="/academy/my-account">My Account</a> | <a href="' 
 			       . wc_logout_url() . '">Logout</a>';
